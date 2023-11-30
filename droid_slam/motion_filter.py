1c9df0804261c59ed080bbebdb71664784837649
Shivam Gautam <shivamgautam@gmail.com>
2023-11-30 12:43:39 -0500

    added logging statements

--- droid_slam/motion_filter.py
+++ droid_slam/motion_filter.py
@@ -8,12 +8,14 @@ from droid_net import DroidNet
 import geom.projective_ops as pops
 from modules.corr import CorrBlock
 
+# from torchvision.utils import flow_to_image
+from matplotlib import pyplot as plt
+
 
 class MotionFilter:
-    """ This class is used to filter incoming frames and extract features """
+    """This class is used to filter incoming frames and extract features"""
 
     def __init__(self, net, video, thresh=2.5, device="cuda:0"):
-        
         # split net modules
         self.cnet = net.cnet
         self.fnet = net.fnet
@@ -26,69 +28,105 @@ class MotionFilter:
         self.count = 0
 
         # mean, std for image normalization
-        self.MEAN = torch.as_tensor([0.485, 0.456, 0.406], device=self.device)[:, None, None]
-        self.STDV = torch.as_tensor([0.229, 0.224, 0.225], device=self.device)[:, None, None]
-        
+        self.MEAN = torch.as_tensor([0.485, 0.456, 0.406], device=self.device)[
+            :, None, None
+        ]
+        self.STDV = torch.as_tensor([0.229, 0.224, 0.225], device=self.device)[
+            :, None, None
+        ]
+
     @torch.cuda.amp.autocast(enabled=True)
     def __context_encoder(self, image):
-        """ context features """
-        net, inp = self.cnet(image).split([128,128], dim=2)
+        """context features"""
+        net, inp = self.cnet(image).split([128, 128], dim=2)
         return net.tanh().squeeze(0), inp.relu().squeeze(0)
 
     @torch.cuda.amp.autocast(enabled=True)
     def __feature_encoder(self, image):
-        """ features for correlation volume """
+        """features for correlation volume"""
         return self.fnet(image).squeeze(0)
 
     @torch.cuda.amp.autocast(enabled=True)
     @torch.no_grad()
     def track(self, tstamp, image, depth=None, intrinsics=None):
-        """ main update operation - run on every frame in video """
+        """main update operation - run on every frame in video"""
 
-        Id = lietorch.SE3.Identity(1,).data.squeeze()
+        Id = lietorch.SE3.Identity(
+            1,
+        ).data.squeeze()
+
+        # The image shape has been downsampled by 8 times for
+        # computing features using the RAFT network
+        # The depth map will be of same dimensions.
+        # This is the size that is being sent for update in DBA layer.
         ht = image.shape[-2] // 8
         wd = image.shape[-1] // 8
 
         # normalize images
-        inputs = image[None, :, [2,1,0]].to(self.device) / 255.0
+        # The shape of the input tensor is [1, 3, 392, 488]
+        # during normalization of the image, the shape is changed to [1, 1, 3, 392, 488]
+        # which is the input to train.
+        inputs = image[None, :, [2, 1, 0]].to(self.device) / 255.0
         inputs = inputs.sub_(self.MEAN).div_(self.STDV)
 
         # extract features
         gmap = self.__feature_encoder(inputs)
 
+        print(f"Motion filter: Image Timestamp: {tstamp}")
         ### always add first frame to the depth video ###
         if self.video.counter.value == 0:
-            net, inp = self.__context_encoder(inputs[:,[0]])
+            net, inp = self.__context_encoder(inputs[:, [0]])
             self.net, self.inp, self.fmap = net, inp, gmap
-            self.video.append(tstamp, image[0], Id, 1.0, depth, intrinsics / 8.0, gmap, net[0,0], inp[0,0])
+            self.video.append(
+                tstamp,
+                image[0],
+                Id,
+                1.0,
+                depth,
+                intrinsics / 8.0,
+                gmap,
+                net[0, 0],
+                inp[0, 0],
+            )
 
         ### only add new frame if there is enough motion ###
-        else:                
+        else:
             # index correlation volume
-            coords0 = pops.coords_grid(ht, wd, device=self.device)[None,None]
-            corr = CorrBlock(self.fmap[None,[0]], gmap[None,[0]])(coords0)
+            coords0 = pops.coords_grid(ht, wd, device=self.device)[None, None]
+            corr = CorrBlock(self.fmap[None, [0]], gmap[None, [0]])(coords0)
 
             # approximate flow magnitude using 1 update iteration
             _, delta, weight = self.update(self.net[None], self.inp[None], corr)
 
             # check motion magnitue / add new frame to video
             if delta.norm(dim=-1).mean().item() > self.thresh:
+                print(
+                    f"Motion filter: Adding frame timstamp - {tstamp} to video buffer"
+                )
                 self.count = 0
-                net, inp = self.__context_encoder(inputs[:,[0]])
+                net, inp = self.__context_encoder(inputs[:, [0]])
                 self.net, self.inp, self.fmap = net, inp, gmap
-                self.video.append(tstamp, image[0], None, None, depth, intrinsics / 8.0, gmap, net[0], inp[0])
+                self.video.append(
+                    tstamp,
+                    image[0],
+                    None,
+                    None,
+                    depth,
+                    intrinsics / 8.0,
+                    gmap,
+                    net[0],
+                    inp[0],
+                )
 
             else:
                 self.count += 1
-
-
 
 
 # class MotionFilter:
 #     """ This class is used to filter incoming frames and extract features """
 
 #     def __init__(self, net, video, thresh=2.5, device="cuda:0"):
-        
+
 #         # split net modules
 #         self.cnet = net.cnet
 #         self.fnet = net.fnet
@@ -103,7 +141,7 @@ class MotionFilter:
 #         # mean, std for image normalization
 #         self.MEAN = torch.as_tensor([0.485, 0.456, 0.406], device=self.device)[:, None, None]
 #         self.STDV = torch.as_tensor([0.229, 0.224, 0.225], device=self.device)[:, None, None]
-        
+
 #     @torch.cuda.amp.autocast(enabled=True)
 #     def __context_encoder(self, image):
 #         """ context features """
@@ -139,7 +177,7 @@ class MotionFilter:
 #             self.video.append(tstamp, image, Id, 1.0, intrinsics / 8.0, gmap[0], net[0], inp[0])
 
 #         ### only add new frame if there is enough motion ###
-#         else:                
+#         else:
 #             # index correlation volume
 #             coords0 = pops.coords_grid(ht, wd, device=self.device)[None,None]
 #             corr = CorrBlock(self.fmap[None], gmap[None])(coords0)
@@ -156,4 +194,3 @@ class MotionFilter:
 
 #             else:
 #                 self.count += 1
-
