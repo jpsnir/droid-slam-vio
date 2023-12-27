ed91e9cfc28682da40fc4ef242ce3b945504d1fd
Shivam Gautam <shivamgautam@gmail.com>
2023-12-27 12:53:08 -0500

    logging capability complete

--- droid_slam/droid_frontend.py
+++ droid_slam/droid_frontend.py
@@ -57,6 +57,8 @@ class DroidFrontend:
 
         self.count += 1
         self.t1 += 1
+        print(f"DROID FRONT END update: count = {self.count}, t1= {self.t1}")
+        print(f"Video Buffer counter : {self.video.counter.value} ")
 
         if self.graph.corr is not None:
             self.graph.rm_factors(self.graph.age > self.max_age, store=True)
@@ -88,21 +90,59 @@ class DroidFrontend:
 
         if d.item() < self.keyframe_thresh:
             self.graph.rm_keyframe(self.t1 - 2)
-
+            print(
+                f"DROID FRONTEND - removing keyframe {self.t1 -2}, motion - {d.item()} less \
+                    than threshold {self.keyframe_thresh}"
+            )
             with self.video.get_lock():
                 self.video.counter.value -= 1
                 self.t1 -= 1
 
         else:
             for itr in range(self.iters2):
+                print(f"DROID FRONTEND update iteration - phase2 - {itr}")
                 self.graph.update(None, None, use_inactive=True)
 
+        self.log_factor_graphs(format=self.save_fg_fmt)
         # set pose for next itration
         self.video.poses[self.t1] = self.video.poses[self.t1 - 1]
         self.video.disps[self.t1] = self.video.disps[self.t1 - 1].mean()
 
         # update visualization
         self.video.dirty[self.graph.ii.min() : self.t1] = True
+
+    def log_factor_graphs(self, format: str = "json"):
+        """
+        log the factor graph data in binary or ascii format
+        """
+        # t1 - 1 is the last frame
+        print(
+            f"Tstamp of last frame {self.t1 - 1} : {self.video.tstamp[self.t1 -1].item()}"
+        )
+
+        filename = f"fg_{self.count:04}_id_{int(self.video.tstamp[self.t1 -1].item()):04}.{format}"
+        factor_graph_data_dict = {
+            "id": self.video.tstamp[self.t1 - 1].cpu(),
+            "intrinsics": self.video.intrinsics[0].cpu(),
+            "graph_data": {
+                "ii": self.graph.ii_total.cpu(),
+                "jj": self.graph.jj_total.cpu(),
+            },
+            "disps": self.video.disps[: self.t1].cpu(),
+            "c_map": self.graph.weight_total.cpu(),
+            "predicted": self.graph.target_total.cpu(),
+            "poses": self.video.poses[: self.t1].cpu(),
+            "tstamp": self.video.tstamp[: self.t1].cpu(),
+        }
+        if format == "pt":
+            data_container = torch.jit.script(
+                FactorGraphContainer(factor_graph_data_dict)
+            )
+            data_container.save(self.save_fg_path.joinpath(filename))
+        if format == "pkl":
+            with open(self.save_fg_path.joinpath(filename), "wb") as f:
+                pickle.dump(factor_graph_data_dict, f)
+            f.close()
 
     def __initialize(self):
         """initialize the SLAM system"""
